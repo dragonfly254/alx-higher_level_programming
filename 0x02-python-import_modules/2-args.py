812606df0cb241ef859f1626f684154ec11f669a
Edwin Nchoga <edwinchoga@gmail.com>
2022-12-01 04:26:12 -0800

    debugged task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 def print_args(argv):
     n = len(argv) - 1
     if n == 0:
-        print("{:d} argument.".format(n))
+        print("{:d} arguments.".format(n))
         return
     if n == 1:
         print("{:d} argument:".format(n))
