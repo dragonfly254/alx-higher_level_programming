5c36e98e28d37213097678dee0b440afbe309b56
Edwin Nchoga <edwinchoga@gmail.com>
2022-12-05 06:47:50 -0800

    debugged task 3, 6, 13

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,5 +6,7 @@ def print_reversed_list_integer(my_list=[]):
         my_list: input list.
 
     """
+    if my_list is none:
+        return
     for num in my_list[::-1]:
         print("{:d}".format(num))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
         matrix: input matrix.
     """
     for row in matrix:
-            print(" ".join("{:d}".format(col) for col in row))
+        print(" ".join("{:d}".format(col) for col in row))
