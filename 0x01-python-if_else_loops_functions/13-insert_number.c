d289ba11156b110c490ca4337986dcd0db024fe1
Edwin Nchoga <edwinchoga@mail.com>
2024-01-03 02:42:09 -0800

    reimplement insert_node in 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,53 +1,41 @@
-#include <stdlib.h>
 #include "lists.h"
 /**
- * insert_node -  inserts a number into a sorted singly linked list.
- * @head: head of the linked list.
- * @number: number to insert.
+ * insert_node - inserts a number into a sorted singly linked list.
+ * @head: head to the linked list.
+ * @number: number to be inserted.
  *
- * Return: the address of the new node, or NULL if it failed.
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *cur;
-	listint_t *prev;
-	listint_t *tmp = malloc(sizeof(listint_t));
-
-	if (!tmp)
+	listint_t *tmp = (listint_t *)malloc(sizeof(listint_t));
+	
+	if (tmp == NULL)
+	{
+		perror("error in mallocing");
 		return (NULL);
-
-	if (*head == NULL)
-	{
-		tmp->n = number;
-		*(head) = tmp;
-		tmp->next = NULL;
 	}
 
-	cur = *(head);
-	prev = *(head);
-
-	if (cur->n > number)
-	{
-		tmp->n = number;
-		tmp->next = cur;
-		head = &tmp;
-	}
-
-	while (cur)
-	{
-		if (cur->n > number)
-		{
-			tmp->n = number;
-			prev->next = tmp;
-			tmp->next = cur;
-
-			return tmp;
-		}
-		prev = cur;
-		cur = cur->next;
-	}
-	prev->next = tmp;
 	tmp->n = number;
 	tmp->next = NULL;
+
+	if (*head == NULL || *head->n >= number)
+	{
+		tmp->next = *head;
+		*head = tmp;
+	}
+	else
+	{
+		cur = *head;
+
+		while (cur->next != NULL && cur->next->n < number)
+		{
+			cur = cur->next;
+		}
+		tmp->next = cur->next;
+		cur->next = tmp;
+	}
+
 	return (tmp);
 }
